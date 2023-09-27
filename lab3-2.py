3eaa79b775efabab33e68e2a9d1c83c621fb3dfb
naz <p24nthompson@fairfieldprep.org>
2023-09-27 11:41:56 -0400

    lab 3-2 all done

--- lab3-2.py
+++ lab3-2.py
@@ -12,3 +12,20 @@ After you have commented out a line, add a comment listing what error type occur
 	print("I'm a happy camper!)
 
 """
+"int(a)"
+"name error"
+"create statement and define a"
+	
+"int('a')"
+"vale erro occured"
+"remove parenthesis so code inst seen comment"
+"'a' + 2"
+	
+"import date"
+"module not found erro"
+"create a module for date"
+
+	
+"print("I'm a happy camper!)"
+"syntax error"
+"inculde missing character which is parenthesis"
